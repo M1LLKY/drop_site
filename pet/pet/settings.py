d7f3d7a3f168b07a649ad159e28e83917517c778
Ivan <m1llky@bk.ru>
2024-10-17 21:19:29 +0300

    DB changed from SQLite3 to PostgreSQL

--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pet_db',
+        'USER': 'pet_admin',
+        'PASSWORD': '1i9v6a8n',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
